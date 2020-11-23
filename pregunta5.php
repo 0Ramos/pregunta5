e1037662e886ba7de40fff513832cacc76202c64
0Ramos <66374859+0Ramos@users.noreply.github.com>
2020-11-23 09:56:06 -0500

    no termino

---
+++ pregunta5.php
@@ -0,0 +1,11 @@
+<?php
+
+class date 
+{                          
+    public $day;  //1 al 31     
+    public $month; //1 al 12
+    public $year; //de 2000n adelante      
+    
+    public function __construct()
+    
+
